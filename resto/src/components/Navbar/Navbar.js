d24bec495a00162de433d0a97db096727ea0f0fe
vincenttommi <vincenttommi@gmail.com>
2023-05-12 00:37:20 +0300

    styling navbar in react

---
+++ resto/src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from 'react-icons/md'
+import './Navbar.css';
+import images from '../../constants/images';
+const Navbar = () => (
+  <nav className='app_navbar'>
+  <div className='app_navbar-logo'>
+    <img src={images.gericht} alt='app logo'/>
+  </div>
+  <ul className='app_navbar_links'>
+  <li className='p_opensans'><a href='#home'>Home</a> </li>
+  <li className='p_opensans'><a href='#about'>About</a> </li>
+  <li className='p_opensans'><a href='#menu'>Menu</a> </li>
+  <li className='p_opensans'><a href='#awards'>Awards</a> </li>
+  <li className='p_opensans'><a href='#contact'>Contact</a> </li>
+
+
+
+
+  
+
+
+
+ 
+  </ul>
+  </nav>
+);
+
+export default Navbar;
