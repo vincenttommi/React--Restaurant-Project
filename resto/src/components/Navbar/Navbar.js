9c3a326b86dac79bcd7850aac840c840f8b0c22a
vincenttommi <vincenttommi@gmail.com>
2023-05-12 01:47:53 +0300

    adding changes tocss

--- resto/src/components/Navbar/Navbar.js
+++ resto/src/components/Navbar/Navbar.js
@@ -24,6 +24,38 @@ const Navbar = () => (
 
 <a href='#login' className='p_opensans'>Book Table</a>
   </div>
+
+<div className='app_navbar-smallscreen'>
+
+  <GiHamburgerMenu color='#fff' fontSize={27}  onClick={()=>{}}></GiHamburgerMenu>
+
+</div>
+
+{/* another div */}
+
+<div className='app_navbar-smallscreen_overlay flex_center slide_bottom'>
+ <MdOutlineRestaurantMenu fontSize={27} className='overlay_close' onClick={()=>{}}></MdOutlineRestaurantMenu>
+
+
+
+
+<ul className='app_navbar_smallscreen'>
+  <li className='p_opensans'><a href='#home'>Home</a> </li>
+  <li className='p_opensans'><a href='#about'>About</a> </li>
+  <li className='p_opensans'><a href='#menu'>Menu</a> </li>
+  <li className='p_opensans'><a href='#awards'>Awards</a> </li>
+  <li className='p_opensans'><a href='#contact'>Contact</a> </li>
+
+  </ul>
+
+
+
+
+
+
+</div>
+
+
   </nav>
 );
 
