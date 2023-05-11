c9a129b0db9178f4594d6210972b984868e14383
vincenttommi <vincenttommi@gmail.com>
2023-05-11 23:27:05 +0300

    Components in react

--- resto/src/App.js
+++ resto/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div>
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
 
-        <h1>Hello</h1>
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
-
-
-    </div>
-  )
-}
-
-export default App
+export default App;
