cf87a458d0f8b529066769508810a3c5c277c4bf
vincenttommi <vincenttommi@gmail.com>
2023-06-12 21:44:24 +0300

    updating chefs section

--- resto/src/container/Chef/Chef.js
+++ resto/src/container/Chef/Chef.js
@@ -1,45 +1,37 @@
 import React from 'react'
-import './Chef.css';
-import { SubHeading } from '../../components';
 
-import {images} from '../../constants';
- const Chef = () => {
+import  { SubHeading } from '../../components'
+import { images } from '../../constants';
+import  './Chef.css';
+
+const Chef = () => {
   return (
- 
-
-
     <div className='app__bg app__wrapper section__padding'>
-      <div className='app__wrapper_img app__wrapper_img-reverse' >
-        <img  src={images.chef} alt='chef'/>
- 
+      <div className='app__wrapper_img app__wrapper_img-reverse'>
+
+        <img src={images.chef} alt='chef'/>
 
       </div>
-    
-    <div className='app__wrapper_info'>
-      {/* introducing attritubes to a component */}
-     <SubHeading  title="Chef's Word"/>
-     <h1 className='headtext__cormorant '>What we believe in</h1>
+      <div className='app__wrapper_info'>
+
+     <SubHeading title="Chef's Word " />
+     <h1 className='headtext__cormorant'>What we believe in</h1>
 
      <div className='app__chef-content'>
       <div className='app__chef-content_quote'>
-
-         <img src={images.quote} alt='quote'/>
-         <p className='p__opensans'>Whatever you do, do with determination. You have one life to live; do your work with passion and give your best. Whether you want to be a chef, doctor, actor, or a mother, be passionate to get the best result.</p>
-
+        <img src={images.quote} alt='quote' />
+        <p className='p__opensans'>Lorem ipsum, placeholder or dummy text used in typesetting and graphic design for previewing layouts. It features scrambled Latin text, which emphasizes the design over content of the layout. It is the standard placeholder text of the printing and publishing industries.</p> 
+      </div>
+      <p className='p__opensans'>Lorem ipsum, placeholder or dummy text used in typesetting and graphic design for previewing layouts. It features scrambled Latin text, which emphasizes the design over content of the layout. It is the standard placeholder text of the printing and publishing industries.</p>
+     </div>
        <div className='app__chef-sign'>
         <p>Kevin Luo</p>
-        <p className='p__opensans'>Chef & Founder </p>
-        <img src={images.sign} alt='sign' />
-
-
-
-
+        <p className='p__opensans'>Chef & Founder</p>
+        <img  src={images.sign} alt="sign"/>
        </div>
       </div>
-     </div>
-    </div>
     </div>
   )
-} 
+}
 
-export default Chef;
+export default Chef 
